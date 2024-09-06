b4c0ab763a56180ca2ba297290873e9c05b418f7
Michael Ray <michaelray2809@yahoo.com>
2024-09-06 04:18:57 +0000

    code that links specific google sheet to script

--- run.py
+++ run.py
@@ -3,3 +3,20 @@ import time
 
 # 3rd party
 from colorama import fore
+
+# Bringing in modules needed to link file to google doc
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('MR - Project 3')
+
+score_sheet = SHEET.worksheet('results')
