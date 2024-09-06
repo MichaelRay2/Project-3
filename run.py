a8f724b16257311c6a8d25f446a0da27d8eceb30
Michael Ray <michaelray2809@yahoo.com>
2024-09-06 04:24:00 +0000

    Added game title

--- run.py
+++ run.py
@@ -18,5 +18,16 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('MR - Project 3')
-
+# setting score_sheet variable to the sheet where all results will be stored
 score_sheet = SHEET.worksheet('results')
+
+# Using Fore and sleep time to make a game title
+def head():
+    print(Fore.RED + "=========================================================================================")
+    time.sleep(1)        
+    print(Fore.BLUE + "  |==||    // \\   ====== ======  ||     ||===  ||===  ||  ||  ==||==  ||==||  ||===      ")
+    print(Fore.BLUE + "  |__||   //===\\    ||     ||    ||     ||===  ||=||  ||==||    ||    ||==||  ||=||  ")
+    print(Fore.BLUE + "  |--||  //     \\   ||     ||    ||===  ||===  ===||  ||  ||  ==||==  ||      ===||    ")
+    print(Fore.BLUE + "  |==||                                       ")
+    time.sleep(1)           
+    print(Fore.RED + "==========================================================================================")
